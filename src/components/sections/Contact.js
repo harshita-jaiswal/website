a9321f273a0c89396e34bd077b96bf0c195f175e
harshita-jaiswal <harshu.jaiswal26@gmail.com>
2019-07-19 18:54:48 +0530

    feat(contact): created contact section

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Github from '../../assets/github.svg';
+import LinkedIn from '../../assets/linkedIn.svg';
+import Codepen from '../../assets/codepen.svg';
 
 // const Container = styled.div`
 // 	width: 100%;
@@ -15,30 +18,61 @@ const Body = styled.div`
 	height: 12vh;
 	display: flex;
 	align-items: center;
-	justify-content: center;
+	justify-content: space-between;
 `;
 const Info = styled.div`
 	/* margin: 2rem auto; */
 	max-width: 1440px;
 	font-size: 0.89rem;
 	position: relative;
-	height: 100%;
+	/* height: 100%; */
 	padding-left: 4rem;
 	color: #ffffff;
 	opacity: 0.8;
 `;
 
-const Detail = styled.span`padding-left: 0.7rem;`;
+const ContainerIc = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-right: 30px;
+`;
+
+const Icon = styled.span`
+	height: 20px;
+	width: 20px;
+	color: #e5e5e5;
+	margin-right: 35px;
+	cursor: pointer;
+	transition: transform 0.3s;
+	transform: scale(1);
+	&:hover {
+		transform: scale(1.25);
+	}
+`;
+
+const Detail = styled.span`padding-left: 0.19rem;`;
 
 export default class Contact extends Component {
 	render() {
 		return (
 			<Body>
 				<Info>
-					<p>
-						Contact: <Detail>8971025107 ∙ harshitajaiswal2693@gmail.com</Detail>
-					</p>
+					{/* <p> */}
+					Contact: <Detail>8971025107 ∙ harshitajaiswal2693@gmail.com</Detail>
+					{/* </p> */}
 				</Info>
+				<ContainerIc>
+					<Icon>
+						<Github />
+					</Icon>
+					<Icon>
+						<LinkedIn />
+					</Icon>
+					<Icon>
+						<Codepen />
+					</Icon>
+				</ContainerIc>
 			</Body>
 		);
 	}
